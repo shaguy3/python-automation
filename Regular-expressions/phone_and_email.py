a3c5f0fde712774a17d868b2e9b5bb34f3869cf8
guy <shaguy3@gmail.com>
2018-12-23 19:39:32 +0200

    Regex project version 1.0

--- Regular-expressions/phone_and_email.py
+++ Regular-expressions/phone_and_email.py
@@ -1,23 +1,38 @@
-import re, pyperclip
+import re
 
 phone_regex = re.compile(r'''
-                        (\d{3}|\d{2})       #First digits
-                        (\s|-)?            #Seperator
-                        (\d{7})             #Main number
+                        (\d{3}|\d{2})        #First digits
+                        (\s|-)?              #Seperator
+                        (\d{7})              #Main number
                         ''', re.VERBOSE)
 
 email_regex = re.compile(r'''
-                        ([a-zA-Z0-9]+)      #Username
-                        (@)                 #Seperator @
-                        ([a-zA-Z0-9.-]+)    #Domain name
-                        (\.[a-zA-Z]{2,4})   #Suffix
+                        ([a-zA-Z0-9.]+)      #Username
+                        (@)                  #Seperator @
+                        ([a-zA-Z0-9.]+)      #Domain name
+                        (\.[a-zA-Z.]{2,4})   #Suffix
                         ''', re.VERBOSE)
 
-test = 'Hello. my name is guy. My phone number is: 054-7941734 and my email address is: shaguy3@gmail.com'
-phone_number = phone_regex.search(test)
-email_address = email_regex.search(test)
-print('Phone number found:', phone_number.group() + '. Email address found:', email_address.group() + '.')
+results_phones = []
+results_emails = []
 
-# TODO: Find matches in clipboard.
+with open('text_source.txt', 'r') as file:
+    phones = phone_regex.findall(file.read())
+    for phone in phones:
+        current_phone = ''
+        for group in phone:
+            current_phone += group
+        results_phones.append(current_phone)
+    file.close()
+with open('text_source.txt', 'r') as file:
+    emails = email_regex.findall(file.read())
+    for email in emails:
+        current_email = ''
+        for group in email:
+            current_email += group
+        results_emails.append(current_email)
+    file.close()
 
-# TODO: Copy results to the clipboard.
+
+print('Phones found: ' + str(results_phones))
+print('emails found: ' + str(results_emails))
