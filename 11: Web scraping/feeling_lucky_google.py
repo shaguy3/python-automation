8be95a2b5e0822ca49b688cb4a85d1cf249ffc1c
guy <shaguy3@gmail.com>
2019-01-02 22:17:38 +0200

    -Made a Utilities package -Started work on Google lucky search

--- 11: Web scraping/feeling_lucky_google.py
+++ 11: Web scraping/feeling_lucky_google.py
@@ -1,7 +1,25 @@
-# TODO: Get an input to search
+import bs4
+import requests
+import webbrowser
+import lxml
 
 # TODO: Open google website
 
 # TODO: Search the requested input
 
 # TODO: Open the first five links of the search in the browser
+
+search = input('What would you like to search? ')
+search = search.replace(' ', '+')
+# Got the input from the user
+
+res = requests.get('https://www.google.com/search?q=' + search)
+res.raise_for_status()
+# Got a response object from the google search
+
+soup = bs4.BeautifulSoup(res.text, features='lxml')
+# Made a beautifulSoup object with the module
+
+links = soup.select('.r a')
+
+print(links)
