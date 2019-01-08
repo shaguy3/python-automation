7ffb186ff42297252c6451c0340f3f1178b1bcb2
guy <shaguy3@gmail.com>
2019-01-08 21:36:52 +0200

    - Started work on selenium_testing

--- 11: Web scraping/selenium_testing.py
+++ 11: Web scraping/selenium_testing.py
@@ -6,7 +6,7 @@ try:
     browser.get('http://www.google.com')
     browser.find_element_by_name('q').send_keys('ירדן יפת')
     browser.find_element_by_name('btnK').submit()
-    browser.find_elements_by_class_name('LC20lb')[0].click()
+    browser.find_elements_by_class_name('LC20lb')[1].click()
 except Exception as err:
     print(err)
 
