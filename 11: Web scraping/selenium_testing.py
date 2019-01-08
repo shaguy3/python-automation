760539a8a077ced50a3190da654194b4bcbe5311
guy <shaguy3@gmail.com>
2019-01-08 21:21:42 +0200

    - Started work on selenium_testing

---
+++ 11: Web scraping/selenium_testing.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+
+try:
+    browser.get('http://www.google.com')
+    browser.find_element_by_name('q').send_keys('ירדן יפת')
+    browser.find_element_by_name('btnK').submit()
+    browser.find_elements_by_class_name('LC20lb')[0].click()
+except Exception as err:
+    print(err)
+
